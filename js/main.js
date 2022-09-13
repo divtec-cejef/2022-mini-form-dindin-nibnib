7094a6b05c0404be3c86f9e71610b85a7a941a6f
dindin-nibnib <odinbeuchat.ob@ikmail.com>
2022-09-13 16:39:43 +0200

    changement de l'animation, ajout d'une alert

--- js/main.js
+++ js/main.js
@@ -7,8 +7,16 @@
 
 console.log('Test liaison JS');
 const backgroundSelector = document.getElementById("background");
+const form = document.querySelector("form");
 backgroundSelector.addEventListener("change", () => {
 	const background = backgroundSelector.value;
 	const body = document.body;
 	body.style.backgroundImage = `url(./img/backgrounds/${background})`;
+});
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const submitButton = form.querySelector('button[type="submit"]');
+	submitButton.classList.add("animate__animated", "animate__heartBeat");
+	alert("Compte enregistré !");
+	console.log("ran");
 });
