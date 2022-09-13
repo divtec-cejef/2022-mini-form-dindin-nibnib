90efb35290ac44869eff65a965950a75e1031c24
dindin-nibnib <odinbeuchat.ob@ikmail.com>
2022-09-13 16:05:20 +0200

    dynamic background

--- js/main.js
+++ js/main.js
@@ -6,3 +6,9 @@
 'use strict';
 
 console.log('Test liaison JS');
+const backgroundSelector = document.getElementById("background");
+backgroundSelector.addEventListener("change", () => {
+	const background = backgroundSelector.value;
+	const body = document.body;
+	body.style.backgroundImage = `url(./img/backgrounds/${background})`;
+});
